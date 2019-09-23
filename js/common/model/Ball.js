829629af950066dc9e63cc85e8438313ae30762b
Martin Veillette <martin.veillette@gmail.com>
2019-09-22 21:05:22 -0400

    use standard Vector2 rather than TwoVector

--- js/common/model/Ball.js
+++ js/common/model/Ball.js
@@ -9,7 +9,6 @@ define( require => {
 
   // modules
   const collisionLab = require( 'COLLISION_LAB/collisionLab' );
-  const TwoVector = require( 'COLLISION_LAB/common/model/TwoVector' );
 
   /**
    * @constructor
@@ -18,33 +17,24 @@ define( require => {
 
     /**
      * @param {number} mass
-     * @param {TwoVector} position
-     * @param {TwoVector} velocity
+     * @param {Vector2} position
+     * @param {Vector2} velocity
      */
     constructor( mass, position, velocity ) {
 
       this.setBall( mass, position, velocity );
     }
 
-    // @public resets the model
-    reset() {
-      //TODO Reset things here.
-    }
-
-    //following to used to reset existing ball object to initial configuration in Model
-
     /**
-     *
-     * @param {number}mass
-     * @param {TwoVector} position
-     * @param {TwoVector} velocity
+     * Sets the parameter for the ball
+     * @param {number} mass
+     * @param {Vector2} position
+     * @param {Vector2} velocity
      */
     setBall( mass, position, velocity ) {
-      this.mass = mass;
-      this.radius = 0.15 * Math.pow( this.mass, 1 / 3 );
+      this.setMass( mass );
       this.position = position;
       this.velocity = velocity;
-      this.momentum = new TwoVector( this.mass * this.velocity.getX(), this.mass * this.velocity.getY() );
     }
 
     /**
@@ -54,13 +44,6 @@ define( require => {
     setMass( mass ) {
       this.mass = mass;
       this.radius = 0.15 * Math.pow( this.mass, 1 / 3 );
-    }
-
-    /**
-     * Resets position to position at previous timestep
-     */
-    backupOneStep() {
-      this.position.setXY( this.position.getXLast(), this.position.getYLast() );
     }
 
     /**
@@ -80,28 +63,34 @@ define( require => {
     }
 
     /**
-     * Get the momentum of the ball
-     * @returns {TwoVector}
+     * Gets the momentum of the ball
+     * @returns {Vector2}
      */
     getMomentum() {
-      this.momentum.setXY( this.mass * this.velocity.getX(), this.mass * this.velocity.getY() );
-      return this.momentum;
+      return this.velocity.timesScalar( this.mass );
     }
 
     /**
-     *  Gets the kinetic energy of the ball
+     * Gets the first moment of the ball (position times mass)
+     * @returns {Vector2}
+     */
+    getFirstMoment() {
+      return this.position.timesScalar( this.mass );
+    }
+
+    /**
+     * Gets the kinetic energy of the ball
      * @returns {number}
      */
     getKE() {
-      const speed = this.velocity.getMagnitude();
-      return 0.5 * this.mass * speed * speed;
+      return 0.5 * this.mass * this.velocity.getMagnitudeSquared();
     }
 
     /**
      * Reverses the velocity of the ball
      */
     reverseVelocity() {
-      this.velocity.flipVector();
+      this.velocity.multiplyScalar( -1 );
     }
 
   }
