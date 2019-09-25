ba40eeaa55078eb971ac2b60060eee12aaec7828
Brandon Li <brandon.li820@icloud.com>
2019-09-25 13:48:03 -0600

    (#15) add the properties for a ball and #16 use getters/etters

--- js/common/model/Ball.js
+++ js/common/model/Ball.js
@@ -1,104 +1,80 @@
 // Copyright 2019, University of Colorado Boulder
 
 /**
- * Ball object
+ * A Ball is the model for all types of balls.
+ *
  * @author Martin Veillette
+ * @author Brandon Li
  */
+
 define( require => {
   'use strict';
 
   // modules
   const collisionLab = require( 'COLLISION_LAB/collisionLab' );
+  const NumberProperty = require( 'AXON/NumberProperty' );
+  const Vector2Property = require( 'DOT/Vector2Property' );
 
-  /**
-   * @constructor
-   */
   class Ball {
 
     /**
-     * @param {number} mass
-     * @param {Vector2} position
-     * @param {Vector2} velocity
+     * @param {number} mass - initial mass of the ball (kg)
+     * @param {Vector2} position - initial position of the center of the ball
+     * @param {Vector2} velocity - initial velocity of the center of mass of the ball
      */
     constructor( mass, position, velocity ) {
 
-      this.setBall( mass, position, velocity );
+      // @public (read-only) massProperty - Property of the mass of the ball (kg)
+      this.massProperty = new NumberProperty( mass );
+
+      // @public (read-only) positionPropety - Property of the center of the ball's position.
+      this.positionPropety = new Vector2Property( position );
+
+      // @public (read-only) velocityProperty - Property of the velocity of the center of mass of the ball.
+      this.velocityProperty = new Vector2Property( velocity );
+
+      //----------------------------------------------------------------------------------------
+      // Handle the changing radius of the Ball based on the mass
+
+      // @public (read-only) - Property of the radius (model) of the ball set to an arbitrary number (to be updated)
+      this.radiusProperty = new NumberProperty( 0 );
+
+      const updateRadiusListener = mass => {
+        // The radius of the ball is a function of the mass
+        this.radius = 0.15 * Math.pow( this.mass, 1 / 3 );
+      }
+      this.massProperty.link( updateRadiusListener );
+
+      //----------------------------------------------------------------------------------------
+      // Handle dispose
+      this.disposeBall = () => {
+        this.massProperty.unlink( this.radiusProperty );
+      }
     }
 
     /**
-     * Sets the parameter for the ball
+     * Sets the mass of the ball.
+     * @public
      * @param {number} mass
-     * @param {Vector2} position
-     * @param {Vector2} velocity
      */
-    setBall( mass, position, velocity ) {
-      this.setMass( mass );
-      this.position = position;
-      this.velocity = velocity;
+    set mass( mass ) {
+      this.massProperty.vaue = mass;
     }
 
     /**
-     * Sets the mass of the ball
-     * @param {number} mass
-     */
-    setMass( mass ) {
-      this.mass = mass;
-      this.radius = 0.15 * Math.pow( this.mass, 1 / 3 );
-    }
-
-    /**
-     * Gets the mass of ball
+     * Gets the mass.
+     * @public
      * @returns {number}
      */
-    getMass() {
-      return this.mass;
-    }
-
-    /**
-     * Gets the radius of the ball
-     * @returns {number}
-     */
-    getRadius() {
-      return this.radius;
-    }
-
-    /**
-     * Gets the momentum of the ball
-     * @returns {Vector2}
-     */
-    getMomentum() {
-      return this.velocity.timesScalar( this.mass );
-    }
-
-    /**
-     * Gets the first moment of the ball (position times mass)
-     * @returns {Vector2}
-     */
-    getFirstMoment() {
-      return this.position.timesScalar( this.mass );
-    }
-
-    /**
-     * Gets the kinetic energy of the ball
-     * @returns {number}
-     */
-    getKE() {
-      return 0.5 * this.mass * this.velocity.getMagnitudeSquared();
-    }
-
-    /**
-     * Reverses the velocity of the ball
-     */
-    reverseVelocity() {
-      this.velocity.multiplyScalar( -1 );
-    }
+    get mass() { return this.massProperty.value; }
 
     /**
      * time steps for ballistic (unimpeded) motion
      * @param {number} dt
      */
     step( dt ) {
-      this.position.add( this.velocity.timesScalar( dt ) );
+      // TODO
+      // this.positionPropety.value.add( this.velocity.timesScalar( dt ) );
     }
   }
 
