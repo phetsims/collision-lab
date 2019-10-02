7b0f141b8a4190d6b5df0778c9a334207e04f025
Brandon Li <brandon.li820@icloud.com>
2019-10-02 13:14:48 -0600

    #26 assert in ballnode

--- js/common/view/BallNode.js
+++ js/common/view/BallNode.js
@@ -11,16 +11,18 @@ define( require => {
 
   // modules
   // const ArrowNode = require( 'SCENERY_PHET/ArrowNode' );
-  // const BooleanProperty = require( 'AXON/BooleanProperty' );
   // const Color = require( 'SCENERY/util/Color' );
   // const merge = require( 'PHET_CORE/merge' );
+  const Ball = require( 'COLLISION_LAB/common/model/Ball' );
+  const BooleanProperty = require( 'AXON/BooleanProperty' );
   const Circle = require( 'SCENERY/nodes/Circle' );
   const collisionLab = require( 'COLLISION_LAB/collisionLab' );
+  const CollisionLabColors = require( 'COLLISION_LAB/common/CollisionLabColors' );
+  const CollisionLabConstants = require( 'COLLISION_LAB/common/CollisionLabConstants' );
   const DragListener = require( 'SCENERY/listeners/DragListener' );
+  const ModelViewTransform2 = require( 'PHETCOMMON/view/ModelViewTransform2' );
   const Node = require( 'SCENERY/nodes/Node' );
   const Property = require( 'AXON/Property' );
-  const CollisionLabConstants = require( 'COLLISION_LAB/common/CollisionLabConstants' );
-  const CollisionLabColors = require( 'COLLISION_LAB/common/CollisionLabColors' );
 
   class BallNode extends Node {
 
@@ -32,6 +34,15 @@ define( require => {
      * @param {Object} [options]
      */
     constructor( ball, index, valuesVisibleProperty, modelViewTransform, options ) {
+
+      assert && assert( ball instanceof Ball, `invalid ball: ${ball}` );
+      assert && assert( typeof index === 'number', `invalid index: ${index}` );
+      assert && assert( valuesVisibleProperty instanceof BooleanProperty, `invalid valuesVisibleProperty: ${valuesVisibleProperty}` );
+      assert && assert( modelViewTransform instanceof ModelViewTransform2, `invalid modelViewTransform: ${modelViewTransform}` );
+      assert && assert( !options || Object.getPrototypeOf( options ) === Object.prototype,
+        `Extra prototype on Options: ${options}` );
+
+      //----------------------------------------------------------------------------------------
 
       super();
 
