db3d0b87edfe0f77eb1d1a3c430a06e11131ddcf
Brandon Li <brandon.li820@icloud.com>
2020-05-19 10:31:23 -0600

    #53 move grid functionality to PlayAReaNode

--- js/common/view/PlayAreaNode.js
+++ js/common/view/PlayAreaNode.js
@@ -2,7 +2,7 @@
 
 /**
  * View for the PlayArea, which includes:
- *  - Border and grid lines
+ *  - background, border, and grid lines
  *  - Kinetic Energy Display
  *  - Center of mass
  *
@@ -11,13 +11,23 @@
  * @author Brandon Li
  */
 
-import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
+import Property from '../../../../axon/js/Property.js';
+import Shape from '../../../../kite/js/Shape.js';
+import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
+import Path from '../../../../scenery/js/nodes/Path.js';
+import Rectangle from '../../../../scenery/js/nodes/Rectangle.js';
 import collisionLab from '../../collisionLab.js';
+import CollisionLabColors from '../CollisionLabColors.js';
+import CollisionLabConstants from '../CollisionLabConstants.js';
 import PlayArea from '../model/PlayArea.js';
 import CenterOfMassNode from './CenterOfMassNode.js';
-import GridNode from './GridNode.js';
 import KineticEnergyDisplay from './KineticEnergyDisplay.js';
+
+// constants
+const MINOR_GRIDLINE_SPACING = CollisionLabConstants.MINOR_GRIDLINE_SPACING; // model
+const MAJOR_GRIDLINE_SPACING = MINOR_GRIDLINE_SPACING * CollisionLabConstants.MINOR_GRIDLINES_PER_MAJOR_GRIDLINE; // model
+const PLAY_AREA_BOUNDS = CollisionLabConstants.PLAY_AREA_BOUNDS;
 
 class PlayAreaNode extends Node {
 
@@ -33,42 +43,91 @@ class PlayAreaNode extends Node {
                gridVisibleProperty,
                kineticEnergyVisibleProperty,
                centerOfMassVisibleProperty,
-               numberOfBallsProperty,
                modelViewTransform,
                options ) {
-
     assert && assert( playArea instanceof PlayArea, `invalid playArea: ${playArea}` );
-    assert && assert( _.every( [ gridVisibleProperty,
-      kineticEnergyVisibleProperty,
-      centerOfMassVisibleProperty ], property => property instanceof BooleanProperty ) );
-
-    //----------------------------------------------------------------------------------------
+    assert && assert( gridVisibleProperty instanceof Property && typeof gridVisibleProperty.value === 'boolean', `invalid gridVisibleProperty: ${gridVisibleProperty}` );
+    assert && assert( kineticEnergyVisibleProperty instanceof Property && typeof kineticEnergyVisibleProperty.value === 'boolean', `invalid kineticEnergyVisibleProperty: ${gridVisibleProperty}` );
+    assert && assert( centerOfMassVisibleProperty instanceof Property && typeof centerOfMassVisibleProperty.value === 'boolean', `invalid centerOfMassVisibleProperty: ${gridVisibleProperty}` );
+    assert && assert( modelViewTransform instanceof ModelViewTransform2, `invalid modelViewTransform: ${modelViewTransform}` );
+    assert && assert( !options || Object.getPrototypeOf( options ) === Object.prototype, `Extra prototype on options: ${options}` );
 
     super( options );
 
+    //----------------------------------------------------------------------------------------
+
+    const playAreaViewBounds = modelViewTransform.modelToViewBounds( PLAY_AREA_BOUNDS );
+
+    const background = new Rectangle( playAreaViewBounds, {
+      stroke: CollisionLabColors.GRID_BORDER_COLOR,
+      fill: CollisionLabColors.GRID_BACKGROUND_COLOR,
+      lineWidth: 3
+    } );
+
+    const majorGridLines = new GridLines( MAJOR_GRIDLINE_SPACING, gridVisibleProperty, modelViewTransform, {
+      lineWidth: CollisionLabConstants.MAJOR_GRID_LINE_WIDTH,
+      stroke: CollisionLabColors.MAJOR_GRID_LINE_COLOR
+    } );
+
+    const minorGridLines = new GridLines( MINOR_GRIDLINE_SPACING, gridVisibleProperty, modelViewTransform, {
+      lineWidth: CollisionLabConstants.MINOR_GRID_LINE_WIDTH,
+      stroke: CollisionLabColors.MINOR_GRID_LINE_COLOR
+    } );
 
 
+    // Set the children of the PlayAreaNode in the correct rendering order.
+    this.children = [
+      background,
+      minorGridLines,
+      majorGridLines,
+      new KineticEnergyDisplay( playArea.kineticEnergySumProperty, kineticEnergyVisibleProperty, playAreaViewBounds ),
+      new CenterOfMassNode( playArea.centerOfMass, centerOfMassVisibleProperty, modelViewTransform )
+    ];
+  }
+}
 
-    // create the grid and border of the playArea
-    const gridNode = new GridNode( playArea.grid, gridVisibleProperty, modelViewTransform );
+// Draws vertical and horizontal grid-lines with the given spacing. Used for major and minor axes. Handles visibility.
+class GridLines extends Path {
 
-    // create the grid and border of the playArea
-    const kineticEnergyDisplay = new KineticEnergyDisplay( playArea.kineticEnergySumProperty,
-      kineticEnergyVisibleProperty );
+  /**
+   * @param {number} spacing - the spacing between grid-lines
+   * @param {BooleanProperty} gridVisibleProperty
+   * @param {ModelViewTransform2} modelViewTransform
+   * @param {Object} [options]
+   */
+  constructor( spacing, gridVisibleProperty, modelViewTransform, options ) {
+    assert && assert( typeof spacing === 'number', `invalid spacing: ${spacing}` );
+    assert && assert( gridVisibleProperty instanceof Property && typeof gridVisibleProperty.value === 'boolean', `invalid gridVisibleProperty: ${gridVisibleProperty}` );
+    assert && assert( modelViewTransform instanceof ModelViewTransform2, `invalid modelViewTransform: ${modelViewTransform}` );
+    assert && assert( !options || Object.getPrototypeOf( options ) === Object.prototype, `invalid options: ${options}` );
 
-    // create the center of mass
-    const centerOfMassNode = new CenterOfMassNode( playArea.centerOfMass,
-      centerOfMassVisibleProperty,
-      numberOfBallsProperty,
-      modelViewTransform );
+    //----------------------------------------------------------------------------------------
 
-    this.addChild( gridNode );
-    this.addChild( kineticEnergyDisplay );
-    this.addChild( centerOfMassNode );
+    // Convenience variables
+    const gridMinX = PLAY_AREA_BOUNDS.minX;
+    const gridMaxX = PLAY_AREA_BOUNDS.maxX;
+    const gridMinY = PLAY_AREA_BOUNDS.minY;
+    const gridMaxY = PLAY_AREA_BOUNDS.maxY;
 
-    kineticEnergyDisplay.left = gridNode.left + 5;
-    kineticEnergyDisplay.bottom = gridNode.bottom - 5;
+    const gridLineShape = new Shape();
 
+    // Vertical lines
+    for ( let xValue = gridMinX; xValue <= gridMaxX; xValue += spacing ) {
+      gridLineShape.moveTo( xValue, gridMinY ).verticalLineTo( gridMaxY );
+    }
+
+    // Horizontal lines
+    for ( let yValue = gridMinY; yValue <= gridMaxY; yValue += spacing ) {
+      gridLineShape.moveTo( gridMinX, yValue ).horizontalLineTo( gridMaxX );
+    }
+
+    super( modelViewTransform.modelToViewShape( gridLineShape ), options );
+
+    //----------------------------------------------------------------------------------------
+
+    // Observe when the gridVisibleProperty changes and update visibility. No need to unlink since PlayAreaNodes
+    // (and GridLines) exist for the lifetime of the sim.
+    gridVisibleProperty.linkAttribute( this, 'visible' );
   }
 }
 
