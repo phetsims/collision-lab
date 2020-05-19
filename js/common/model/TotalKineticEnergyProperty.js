451c3934a8b5ca1b60d6f8ba2a1e1b49a036a0ca
Brandon Li <brandon.li820@icloud.com>
2020-05-19 12:46:34 -0600

    Clean up TotalKineticEnergyProperty

--- js/common/model/TotalKineticEnergyProperty.js
+++ js/common/model/TotalKineticEnergyProperty.js
@@ -1,56 +1,76 @@
 // Copyright 2019-2020, University of Colorado Boulder
 
 /**
- * Determine the total kinetic energy of the system
+ * A NumberProperty sub-type that indicates the total kinetic energy of a system of Balls, in J.
+ *
+ * TotalKineticEnergyProperty is created at the start of the sim and is never disposed, so no dispose method
+ * is necessary.
  *
  * @author Martin Veillette
+ * @author Brandon Li
  */
 
 import NumberProperty from '../../../../axon/js/NumberProperty.js';
+import ObservableArray from '../../../../axon/js/ObservableArray.js';
 import collisionLab from '../../collisionLab.js';
+import Ball from './Ball.js';
 
 class TotalKineticEnergyProperty extends NumberProperty {
+
   /**
-   *
    * @param {ObservableArray.<Ball>} balls
    */
   constructor( balls ) {
+    assert && assert( balls instanceof ObservableArray && balls.count( ball => ball instanceof Ball ) === balls.length, `invalid balls: ${balls}` );
 
-    // set the initial value of the kineticEnergyProperty to zero
-    super( 0 );
+    // Set the initial value of the TotalKineticEnergyProperty to zero. To be updated later.
+    super( 0, {
+      isValidValue: value => value >= 0 // Energy is always positive or zero.
+    } );
 
-    // define a function to update the total kinetic energy
-    const updateKineticEnergySum = () => {
+    // @private {ObservableArray.<Ball>} - reference the balls that were passed in.
+    this.balls = balls;
 
-      // @public (read-only) assign the kinetic energy sum to this.value
-      this.value = balls.reduce( 0, ( accumulator, ball ) => {
-        return accumulator + ball.kineticEnergy;
-      } );
-    };
+    //----------------------------------------------------------------------------------------
 
-    const addItemAddedBallListener = addedBall => {
+    // Observe when Balls are added to the system. Never unlinked since the TotalKineticEnergyProperty is never disposed
+    balls.addItemAddedListener( addedBall => {
 
-      // update the kinetic energy sum
-      addedBall.kineticEnergyProperty.link( updateKineticEnergySum );
+      // Update the value of this Property when the addedBall's kineticEnergy changes. Link is removed when the Ball is
+      // removed from the system.
+      const kineticEnergyListener = () => { this.value = this.computeTotalBallKineticEnergy(); };
+      addedBall.kineticEnergyProperty.link( kineticEnergyListener );
 
-      // Observe when the ball is removed to unlink listeners
+      // Observe when the ball is removed to unlink listeners.
       const removeBallListener = removedBall => {
-
-        // update the kinetic energy sum upon the removal of one ball
-        updateKineticEnergySum();
-
         if ( removedBall === addedBall ) {
 
-          // unlink the listener attached to the removedBall.
-          removedBall.kineticEnergyProperty.unlink( updateKineticEnergySum );
+          // Recompute the total kinetic energy now that there is one less Ball in the system.
+          this.value = this.computeTotalBallKineticEnergy();
+
+          // Unlink the listener attached to the removedBall.
+          removedBall.kineticEnergyProperty.unlink( kineticEnergyListener );
 
           balls.removeItemRemovedListener( removeBallListener );
         }
       };
       balls.addItemRemovedListener( removeBallListener );
-    };
+    } );
+  }
 
-    balls.addItemAddedListener( addItemAddedBallListener );
+  /**
+   * Computes the kinetic energy of the Balls in the system.
+   * @private
+   *
+   * @returns {number} - in J.
+   */
+  computeTotalBallKineticEnergy() {
+    let totalKineticEnergy = 0;
+
+    this.balls.forEach( ball => {
+      totalKineticEnergy += ball.kineticEnergy;
+    } );
+    return totalKineticEnergy;
   }
 }
 
