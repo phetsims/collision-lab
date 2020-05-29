3276db81cc7652215dcc7a6f22f97a0aec5ce9e2
Brandon Li <brandon.li820@icloud.com>
2020-05-29 10:22:31 -0600

    #67 add skeletons of MomentaDiagram model classes

---
+++ js/common/model/MomentaDiagram.js
@@ -0,0 +1,18 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ *
+ * @author Brandon Li
+ */
+
+import collisionLab from '../../collisionLab.js';
+
+class MomentaDiagram {
+
+  constructor() {
+
+  }
+}
+
+collisionLab.register( 'MomentaDiagram', MomentaDiagram );
+export default MomentaDiagram;
---
+++ js/common/model/MomentaDiagramVector.js
@@ -0,0 +1,18 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ *
+ * @author Brandon Li
+ */
+
+import collisionLab from '../../collisionLab.js';
+
+class MomentaDiagramVector {
+
+  constructor() {
+
+  }
+}
+
+collisionLab.register( 'MomentaDiagramVector', MomentaDiagramVector );
+export default MomentaDiagramVector;
