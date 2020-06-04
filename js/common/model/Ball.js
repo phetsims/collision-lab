02e6a15b6763a9fb089b3ff82e4fa8866530ea0e
Brandon Li <brandon.li820@icloud.com>
2020-06-04 16:52:53 -0600

    doc

--- js/common/model/Ball.js
+++ js/common/model/Ball.js
@@ -13,7 +13,8 @@
  *   5. Track the kinetic energy of the Ball.
  *   6. Dragging, user-control, restarting, etc.
  *
- * Balls are created at the start of the sim and are never disposed, so no dispose method is necessary.
+ * For the 'Collision Lab' sim, the same Ball instances are used with the same number of Balls. Thus, Balls are created
+ * at the start of the sim and persist for the lifetime of the sim, so no dispose method is necessary.
  *
  * @author Brandon Li
  * @author Martin Veillette
