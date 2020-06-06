7caed34e928b1a6369e3761d3138ef75b2826c51
Brandon Li <brandon.li820@icloud.com>
2020-06-05 22:47:43 -0600

    #69 doc

--- js/common/view/LeaderLinesNode.js
+++ js/common/view/LeaderLinesNode.js
@@ -1,12 +1,12 @@
 // Copyright 2020, University of Colorado Boulder
 
 /**
- * LeaderLinesNode is a specialized view to display guiding dashed lines from a location to the edge of a border.
+ * LeaderLinesNode is a specialized view to display guiding dashed lines from a location to the edge of a bounding box.
  * In the 'Collision Lab' simulation, its purpose is to allow the user to line up Balls within the PlayArea's
  * coordinate system.
  *
- * LeaderLinesNode appears when Balls are being dragged. Contains a method to set the reticle position. They should
- * be instantiated once for every BallNode for multi touch (since dragging multiple Balls at once is allowed). Like
+ * LeaderLinesNode appears when Balls are being dragged. It contains a method to set the reticle position. They should
+ * be instantiated once for every BallNode for multi-touch (since dragging multiple Balls at once is allowed). Like
  * BallNodes, they are created at the start of the sim and are never disposed.
  *
  * @author Brandon Li
@@ -23,11 +23,11 @@ import collisionLab from '../../collisionLab.js';
 class LeaderLinesNode extends Path {
 
   /**
-   * @param {Bounds2} playAreaViewBounds - the Bounds of the PlayArea, in view coordinates.
+   * @param {Bounds2} leaderLinesBounds - the Bounding box of the lines, in the parent-coordinate frame.
    * @param {Object} [options]
    */
-  constructor( playAreaViewBounds, options ) {
-    assert && assert( playAreaViewBounds instanceof Bounds2, `invalid playAreaViewBounds: ${playAreaViewBounds}` );
+  constructor( leaderLinesBounds, options ) {
+    assert && assert( leaderLinesBounds instanceof Bounds2, `invalid leaderLinesBounds: ${leaderLinesBounds}` );
     assert && assert( !options || Object.getPrototypeOf( options === Object.prototype ), `invalid options: ${options}` );
 
     options = merge( {
@@ -41,24 +41,26 @@ class LeaderLinesNode extends Path {
 
     //----------------------------------------------------------------------------------------
 
-    // @private {Bounds2} - reference to the passed-in playAreaViewBounds
-    this.playAreaViewBounds = playAreaViewBounds;
+    // @private {Bounds2} - reference to the passed-in leaderLinesBounds
+    this.leaderLinesBounds = leaderLinesBounds;
   }
 
   /**
    * Sets the position of the reticle (intersection point) of the leader lines, in the parent coordinate-frame.
    * @public
    *
-   * @param {Vector2} position - in view coordinates
+   * @param {Vector2} reticle - in view coordinates
    */
-  set reticle( position ) {
-    assert && assert( position instanceof Vector2 && position.isFinite(), `invalid position: ${position}` );
+  set reticle( reticle ) {
+    assert && assert( reticle instanceof Vector2 && reticle.isFinite(), `invalid reticle: ${reticle}` );
+    assert && assert( this.leaderLinesBounds.contains( reticle ), `reticle out of bounds: ${reticle}` );
 
+    // Update the shape.
     this.shape = new Shape()
-      .moveTo( this.playAreaViewBounds.minX, position.y )
-      .horizontalLineTo( this.playAreaViewBounds.maxX )
-      .moveTo( position.x, this.playAreaViewBounds.minY )
-      .verticalLineTo( this.playAreaViewBounds.maxY )
+      .moveTo( this.leaderLinesBounds.minX, reticle.y )
+      .horizontalLineTo( this.leaderLinesBounds.maxX )
+      .moveTo( reticle.x, this.leaderLinesBounds.minY )
+      .verticalLineTo( this.leaderLinesBounds.maxY )
       .makeImmutable();
   }
 }
