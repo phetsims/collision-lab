5834c7ca218320dc41544b6c65ebb092ec0224b4
Brandon Li <brandon.li820@icloud.com>
2020-06-12 21:26:50 -0600

    doc

--- js/common/view/BallValuesPanelColumnTypes.js
+++ js/common/view/BallValuesPanelColumnTypes.js
@@ -11,15 +11,17 @@ import Enumeration from '../../../../phet-core/js/Enumeration.js';
 import collisionLab from '../../collisionLab.js';
 
 const BallValuesPanelColumnTypes = Enumeration.byKeys( [
-  'BALL_ICONS',
-  'MASS',
-  'MASS_SLIDERS',
-  'X_POSITION',
-  'Y_POSITION',
-  'X_VELOCITY',
-  'Y_VELOCITY',
-  'X_MOMENTUM',
-  'Y_MOMENTUM'
+
+  'BALL_ICONS',   // Column of Ball Icons. For displaying purposes only.
+  'MASS',         // Column of mass NumberDisplays. Editable by the user.
+  'MASS_SLIDERS', // Column of sliders to change the Mass of a Ball. Only shown when 'More Data' is unchecked.
+  'X_POSITION',   // Column of x-position NumberDisplays. Editable by the user.
+  'Y_POSITION',   // Column of y-position NumberDisplays. Editable by the user and shown for 'Explore 2D' only.
+  'X_VELOCITY',   // Column of x-velocity NumberDisplays. Editable by the user.
+  'Y_VELOCITY',   // Column of y-velocity NumberDisplays. Editable by the user and shown for 'Explore 2D' only.
+  'X_MOMENTUM',   // Column of x-momentum NumberDisplays. NOT editable by the user.
+  'Y_MOMENTUM'    // Column of y-momentum NumberDisplays. NOT editable by the user and shown for 'Explore 2D' only.
+
 ] );
 
 collisionLab.register( 'BallValuesPanelColumnTypes', BallValuesPanelColumnTypes );
