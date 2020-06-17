9093e33cdbfd2fa5c8c3e54c5904ebd6f9437c8f
Brandon Li <brandon.li820@icloud.com>
2020-06-17 12:54:46 -0600

    #69 clean up Explore1DScreenView

--- js/explore1D/view/Explore1DScreenView.js
+++ js/explore1D/view/Explore1DScreenView.js
@@ -6,12 +6,14 @@
  * @author BrandonLi
  */
 
-import Vector2 from '../../../../dot/js/Vector2.js';
+import merge from '../../../../phet-core/js/merge.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import collisionLab from '../../collisionLab.js';
 import CollisionLabConstants from '../../common/CollisionLabConstants.js';
+import BallSystemNode from '../../common/view/BallSystemNode.js';
 import CollisionLabControlPanel from '../../common/view/CollisionLabControlPanel.js';
 import CollisionLabScreenView from '../../common/view/CollisionLabScreenView.js';
+import IntroModel from '../../intro/model/IntroModel.js';
 
 class Explore1DScreenView extends CollisionLabScreenView {
 
@@ -20,35 +22,59 @@ class Explore1DScreenView extends CollisionLabScreenView {
    * @param {Tandem} tandem
    * @param {Object} [options]
    */
-  constructor( model, tandem ) {
+  constructor( model, tandem, options ) {
     assert && assert( model instanceof IntroModel, `invalid model: ${model}` );
     assert && assert( tandem instanceof Tandem, `invalid tandem: ${tandem}` );
 
-    // Create the ControlPanel for the 'Explore 1D' screen.
-    const controlPanel = new CollisionLabControlPanel( viewProperties,
+    options = merge( {
+
+      playAreaTop: CollisionLabConstants.PLAY_AREA_VIEW_TOP_1D,
+      playAreaControlSetOptions: {
+        includeGridCheckbox: false
+      }
+    }, options );
+
+    super( model, tandem, options );
+  }
+
+  /**
+   * Creates the CollisionLabControlPanel for the 'Explore 1D' screen. Called in the constructor of the super-class.
+   *
+   * @override
+   * @protected
+   * @param {CollisionLabViewProperties} viewProperties
+   * @param {CollisionLabModel} model
+   * @returns {CollisionLabControlPanel}
+   */
+  createControlPanel( viewProperties, model ) {
+    return new CollisionLabControlPanel(
+      viewProperties,
       model.ballSystem.centerOfMassVisibleProperty,
       model.playArea.reflectingBorderProperty,
       model.playArea.elasticityPercentProperty,
       model.playArea.inelasticCollisionTypeProperty,
-      model.ballSystem.ballsConstantSizeProperty, {
-        elasticityControlSetNodeOptions: {
-          includeStickSlipSwitch: false
-        }
-      } );
-
-    // Create the BallSystemNode for the 'Explore 1D' screen.
-
-    super( model, tandem, {
-      playAreaLeftTop: new Vector2( CollisionLabConstants.PLAY_AREA_LEFT, CollisionLabConstants.PLAY_AREA_VIEW_TOP_1D ),
-      playAreaControlSetOptions: {
-        includeGridCheckbox: false
-      }
-    } );
+      model.ballSystem.ballsConstantSizeProperty
+    );
   }
 
-  // @protected
-  createControlPanel( viewProperties, model ) {
-    return
+  /**
+   * Creates the BallSystemNode for the 'Explore 1D' screen. Called in the constructor of the super-class.
+   *
+   * @override
+   * @protected
+   * @param {CollisionLabModel} model
+   * @param {CollisionLabViewProperties} viewProperties
+   * @param {ModelViewTransform2} modelViewTransform
+   * @returns {BallSystemNode}
+   */
+  createBallSystemNode( model, viewProperties, modelViewTransform ) {
+    return new BallSystemNode( model.ballSystem,
+      model.playArea,
+      viewProperties.valuesVisibleProperty,
+      viewProperties.velocityVectorVisibleProperty,
+      viewProperties.momentumVectorVisibleProperty,
+      model.isPlayingProperty,
+      modelViewTransform );
   }
 }
 
