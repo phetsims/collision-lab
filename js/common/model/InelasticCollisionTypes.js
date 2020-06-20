44d91725e5beeb84e7be8f4e3187b57cb6874c49
Brandon Li <brandon.li820@icloud.com>
2020-06-20 16:55:37 -0600

    #107 doc

--- js/common/model/InelasticCollisionTypes.js
+++ js/common/model/InelasticCollisionTypes.js
@@ -2,10 +2,8 @@
 
 /**
  * Enumeration of the possible 'types' of perfectly inelastic collisions. A perfectly inelastic happens when the
- * elasticity (and the coefficient of restitution) is 0.
- *
- * The documentation in this file is not exhaustive. Please also see ColisionEngine.js and InelasticRotationEngine.js
- * for complete background.
+ * elasticity (and the coefficient of restitution) is 0. The documentation in this file is not exhaustive. Please also
+ * see ColisionEngine.js and InelasticRotationEngine.js for complete background.
  *
  * @author Brandon Li
  */
@@ -16,7 +14,7 @@ import collisionLab from '../../collisionLab.js';
 const InelasticCollisionTypes = Enumeration.byKeys( [
 
   // Perfectly inelastic collisions that 'slip' follow the standard collision-response algorithm, where the component of
-  // velocity along the 'Plane of Contact' is equal before and after the collision. This is described in
+  // velocity along the 'Plane of Contact' is exactly equal before and after the collision. This is described more in
   // http://web.mst.edu/~reflori/be150/Dyn%20Lecture%20Videos/Impact%20Particles%201/Impact%20Particles%201.pdf.
   //
   // This is the only type of perfectly inelastic collisions in the flash simulation. All screens use this as the
@@ -24,7 +22,7 @@ const InelasticCollisionTypes = Enumeration.byKeys( [
   'SLIP',
 
   // Perfectly inelastic collisions that 'stick' are a new feature of the HTML5 version of the simulation, where
-  // Balls completely stick together and rotate around the center of mass of the cluster of Balls if and only if the
+  // Balls completely stick together and rotate around the center of mass of the cluster of Balls, if and only if the
   // collision isn't head-on.
   //
   // The issue of rotations were first discussed in https://github.com/phetsims/collision-lab/issues/3 and
