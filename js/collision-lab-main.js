c78a22dc9b507bf9c3c195b80cb0336ea423ddcb
Brandon Li <brandon.li820@icloud.com>
2020-06-22 15:43:22 -0600

    Clean up

--- js/collision-lab-main.js
+++ js/collision-lab-main.js
@@ -14,11 +14,8 @@ import Explore1DScreen from './explore1D/Explore1DScreen.js';
 import Explore2DScreen from './explore2D/Explore2DScreen.js';
 import IntroScreen from './intro/IntroScreen.js';
 
-const collisionLabTitleString = collisionLabStrings[ 'collision-lab' ].title;
-
 const simOptions = {
   credits: {
-    //TODO fill in credits, all of these fields are optional, see joist.CreditsNode
     leadDesign: '',
     softwareDevelopment: '',
     team: '',
@@ -29,13 +26,13 @@ const simOptions = {
   }
 };
 
-// launch the sim - beware that scenery Image nodes created outside of simLauncher.launch() will have zero bounds
-// until the images are fully loaded, see https://github.com/phetsims/coulombs-law/issues/70
+// Launch the 'Collision Lab' simulation.
 simLauncher.launch( () => {
-  const sim = new Sim( collisionLabTitleString, [
+  const sim = new Sim( collisionLabStrings[ 'collision-lab' ].title, [
     new IntroScreen( Tandem.ROOT.createTandem( 'introScreen' ) ),
     new Explore1DScreen( Tandem.ROOT.createTandem( 'explore1DScreen' ) ),
     new Explore2DScreen( Tandem.ROOT.createTandem( 'explore2DScreen' ) )
   ], simOptions );
+
   sim.start();
 } );
