40953d81944f805cdac53128e94db34d5ab74063
Brandon Li <brandon.li820@icloud.com>
2020-06-21 18:59:26 -0600

    #107 defaults

--- js/common/model/PlayArea.js
+++ js/common/model/PlayArea.js
@@ -76,7 +76,7 @@ class PlayArea {
 
     // @public {EnumerationProperty.<InelasticCollisionTypes} - the type of collision for perfectly inelastic collisions
     this.inelasticCollisionTypeProperty = new EnumerationProperty( InelasticCollisionTypes,
-      InelasticCollisionTypes.STICK );
+      InelasticCollisionTypes.SLIP );
   }
 
   /**
