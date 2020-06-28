5de5c3f37b5d1181263fdb88ddf76655df57554d
Brandon Li <brandon.li820@icloud.com>
2020-06-27 18:34:50 -0600

    #111 clean up Explore1DModel

--- js/explore1D/model/Explore1DModel.js
+++ js/explore1D/model/Explore1DModel.js
@@ -6,65 +6,33 @@
  * @author Brandon Li
  */
 
-import Tandem from '../../../../tandem/js/Tandem.js';
 import collisionLab from '../../collisionLab.js';
-import CollisionLabConstants from '../../common/CollisionLabConstants.js';
-import CollisionEngine from '../../common/model/CollisionEngine.js';
 import CollisionLabModel from '../../common/model/CollisionLabModel.js';
-import PlayArea from '../../common/model/PlayArea.js';
 import Explore1DBallSystem from './Explore1DBallSystem.js';
+import Explore1DPlayArea from './Explore1DPlayArea.js';
 
 class Explore1DModel extends CollisionLabModel {
 
   /**
-   * @param {Tandem} tandem
+   * Creates the PlayArea for the 'Explore1D' screen, which uses a sub-type of PlayArea.
+   * @override
+   * @protected
+   *
+   * @returns {Explore1DPlayArea}
    */
-  constructor( tandem ) {
-    assert && assert( tandem instanceof Tandem, `invalid tandem: ${tandem}` );
-
-    super( tandem, {
-      playAreaOptions: {
-        dimensions: 1,
-        isGridVisibleInitially: true,
-        bounds: PlayArea.DEFAULT_BOUNDS.erodedY( CollisionLabConstants.PLAY_AREA_1D_ERODED_Y )
-      }
-    } );
-
-    //----------------------------------------------------------------------------------------
-
-    assert && this.playArea.gridVisibleProperty.link( gridVisible => assert( gridVisible, 'grids must be visible in Explore 1D' ) );
+  createPlayArea() {
+    return new Explore1DPlayArea();
   }
 
   /**
+   * Creates the BallSystem for the 'Explore1D' screen, which uses a sub-type of BallSystem.
    * @override
-   * Creates the BallSystem for the 'Explore 1D' screen. Called in the constructor of the super-class. For this screen,
-   * this method will instantiate a sub-type of BallSystem: Explore1DBallSystem. It also has its own custom
-   * initial BallStates.
-   *
    * @protected
-   * @param {PlayArea} playArea - the PlayArea instance of the sim.
+   *
    * @returns {Explore1DBallSystem}
    */
-  createBallSystem( playArea ) {
-    assert && assert( playArea instanceof PlayArea, `invalid playArea: ${playArea}` );
-
-    return new Explore1DBallSystem( playArea );
-  }
-
-  /**
-   * @override
-   * Creates the CollisionEngine for the 'Explore 1D' screen. Called in the constructor of the super-class.
-   *
-   * @protected
-   * @param {PlayArea} playArea - the PlayArea instance of the sim.
-   * @param {Explore1DBallSystem} ballSystem - the BallSystem instance of the sim.
-   * @returns {CollisionEngine}
-   */
-  createCollisionEngine( playArea, ballSystem ) {
-    assert && assert( playArea instanceof PlayArea, `invalid playArea: ${playArea}` );
-    assert && assert( ballSystem instanceof Explore1DBallSystem, `invalid ballSystem: ${ballSystem}` );
-
-    return new CollisionEngine( this.playArea, this.ballSystem );
+  createBallSystem() {
+    return new Explore1DBallSystem( this.playArea, this.elapsedTimeProperty );
   }
 }
 
