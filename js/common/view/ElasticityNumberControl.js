4905fa21475ae77fa7ddfe6febf3250afd8be795
Brandon Li <brandon.li820@icloud.com>
2020-06-27 18:03:43 -0600

    #111 ElasticityControlSet -< ElasticityNumberControl

---
+++ js/common/view/ElasticityNumberControl.js
@@ -0,0 +1,99 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * ElasticityNumberControl is a NumberControl sub-type to display and allow the user to manipulate the elasticity
+ * of all all ball collisions in a system. It appears inside of control-panels on all screens.
+ *
+ * The ElasticityNumberControl is labeled with 'Inelastic' and 'Elastic' instead of the percentages as ticks. Some
+ * screens use the enabledRangeProperty to disable perfectly inelastic collisions, while some screens use it to
+ * only allow perfectly inelastic collisions.
+ *
+ * ElasticityNumberControls are created at the start of the sim and are never disposed.
+ *
+ * @author Brandon Li
+ */
+
+import Dimension2 from '../../../../dot/js/Dimension2.js';
+import Utils from '../../../../dot/js/Utils.js';
+import merge from '../../../../phet-core/js/merge.js';
+import AssertUtils from '../../../../phetcommon/js/AssertUtils.js';
+import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
+import NumberControl from '../../../../scenery-phet/js/NumberControl.js';
+import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
+import Text from '../../../../scenery/js/nodes/Text.js';
+import VBox from '../../../../scenery/js/nodes/VBox.js';
+import Color from '../../../../scenery/js/util/Color.js';
+import collisionLab from '../../collisionLab.js';
+import collisionLabStrings from '../../collisionLabStrings.js';
+import CollisionLabConstants from '../CollisionLabConstants.js';
+
+// constants
+const ELASTICITY_PERCENT_RANGE = CollisionLabConstants.ELASTICITY_PERCENT_RANGE;
+const ELASTICITY_PERCENT_INTERVAL = 5;
+
+class ElasticityNumberControl extends VBox {
+
+  /**
+   * @param {Property.<number>} elasticityPercentProperty
+   * @param {Object} [options]
+   */
+  constructor( elasticityPercentProperty, options ) {
+    assert && AssertUtils.assertPropertyOf( elasticityPercentProperty, 'number' );
+
+    options = merge( {
+
+      // {Object} - passed to the tick Text instances.
+      tickTextOptions: {
+        font: new PhetFont( 12 ),
+        maxWidth: 45 // constrain width for i18n, determined empirically
+      },
+
+      // superclass options
+      layoutFunction: NumberControl.createLayoutFunction4(),
+      includeArrowButtons: false,
+      sliderOptions: {
+        constrainValue: value => Utils.roundToInterval( value, ELASTICITY_PERCENT_INTERVAL ),
+        majorTickLength: 5,
+        tickLabelSpacing: 10,
+        thumbSize: new Dimension2( 14, 24 )
+      },
+      numberDisplayOptions: {
+        valuePattern: StringUtils.fillIn( collisionLabStrings.pattern.valueUnits, {
+          units: collisionLabStrings.units.percent
+        } ),
+        textOptions: { font: CollisionLabConstants.DISPLAY_FONT, maxWidth: 90 },
+        backgroundStroke: Color.BLACK,
+        backgroundLineWidth: 0.5
+      },
+      titleNodeOptions: {
+        font: CollisionLabConstants.PANEL_TITLE_FONT,
+        maxWidth: 90 // constrain width for i18n, determined empirically
+      }
+    }, options );
+
+    //----------------------------------------------------------------------------------------
+
+    assert && assert( !options.sliderOptions.majorTicks, 'ElasticityNumberControl sets majorTicks' );
+    assert && assert( !options.sliderOptions.trackSize, 'ElasticityNumberControl sets trackSize' );
+
+    // Create the 'Inelastic' and 'Elastic' tick labels.
+    const inelasticLabel = new Text( collisionLabStrings.inelastic, options.tickTextOptions );
+    const elasticLabel = new Text( collisionLabStrings.elastic, options.tickTextOptions );
+
+    // Compute the width of the track to ensure the NumberControl fits in control-panels.
+    const trackWidth = CollisionLabConstants.CONTROL_PANEL_CONTENT_WIDTH
+                       - ( inelasticLabel.width + elasticLabel.width ) / 2;
+
+    // Set options that cannot be overridden.
+    options.slider.trackSize = new Dimension2( trackWidth, 0.1 );
+    options.sliderOptions.majorTicks = [
+      { value: ELASTICITY_PERCENT_RANGE.min, label: inelasticLabel },
+      { value: ELASTICITY_PERCENT_RANGE.max, label: elasticLabel }
+    ];
+
+    super( options );
+  }
+}
+
+collisionLab.register( 'ElasticityNumberControl', ElasticityNumberControl );
+export default ElasticityNumberControl;
