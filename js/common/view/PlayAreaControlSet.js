87a9a45696566e9792e20f8682dd116048b3ec93
Brandon Li <brandon.li820@icloud.com>
2020-07-06 11:38:10 -0600

    #118 Add option to dis-include number spinner

--- js/common/view/PlayAreaControlSet.js
+++ js/common/view/PlayAreaControlSet.js
@@ -1,9 +1,9 @@
 // Copyright 2020, University of Colorado Boulder
 
 /**
- * A view specialized to display Controls for the user to change the number of Balls in the PlayArea system and if the
- * PlayArea grid is visible. It is positioned just outside the PlayArea on the top-right side and appears on all of the
- * Screens except for the 'Intro' screen.
+ * A view specialized to display Controls for the user to change the number of Balls in the PlayArea system and the
+ * visibility of the PlayArea's Grid. It is positioned just outside the PlayArea on the top-right side and appears on
+ * all of the Screens except for the 'Intro' screen.
  *
  * The PlayAreaControlSet consists of:
  *   - a NumberSpinner to allow the user to change the number of balls in the PlayArea
@@ -16,7 +16,9 @@
 
 import Property from '../../../../axon/js/Property.js';
 import Range from '../../../../dot/js/Range.js';
+import assertMutuallyExclusiveOptions from '../../../../phet-core/js/assertMutuallyExclusiveOptions.js';
 import merge from '../../../../phet-core/js/merge.js';
+import AssertUtils from '../../../../phetcommon/js/AssertUtils.js';
 import GridCheckbox from '../../../../scenery-phet/js/GridCheckbox.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
 import Text from '../../../../scenery/js/nodes/Text.js';
@@ -35,12 +37,23 @@ class PlayAreaControlSet extends VBox {
    * @param {Object} [options]
    */
   constructor( numberOfBallsProperty, numberOfBallsRange, gridVisibleProperty, options ) {
-    assert && assert( numberOfBallsProperty instanceof Property && typeof numberOfBallsProperty.value === 'number', `invalid numberOfBallsProperty: ${numberOfBallsProperty}` );
+    assert && AssertUtils.assertPropertyOf( numberOfBallsProperty, 'number' );
     assert && assert( numberOfBallsRange instanceof Range, `invalid numberOfBallsRange: ${numberOfBallsRange}` );
-    assert && assert( gridVisibleProperty instanceof Property && typeof gridVisibleProperty.value === 'boolean', `invalid gridVisibleProperty: ${gridVisibleProperty}` );
-
+    assert && AssertUtils.assertPropertyOf( gridVisibleProperty, 'boolean' );
+    assert && assertMutuallyExclusiveOptions( options, [ 'includeGridCheckbox' ], [ 'includeNumberOfBallsSpinner' ] );
 
     options = merge( {
+
+      // {boolean} - indicates if components of the PlayAreaControlSet are included (mutually exclusive).
+      includeGridCheckbox: true,
+      includeNumberOfBallsSpinner: true,
+
+      // {number} - y-margins between the title, number spinner, and grid checkbox, respectively.
+      titleToNumberSpinnerMargin: 6,
+      numberSpinnerToGridCheckboxMargin: 12,
+
+      // {Object} - options passed to the GridCheckbox instance.
+      gridCheckboxOptions: merge( { gridSize: 26 }, CollisionLabConstants.CHECKBOX_OPTIONS ),
 
       // {Object} - passed to the title Text instance.
       titleTextOptions: {
@@ -48,7 +61,7 @@ class PlayAreaControlSet extends VBox {
         maxWidth: 40 // constrain width for i18n, determined empirically
       },
 
-      // {Object} - options passed to the NumberSpinner instance
+      // {Object} - options passed to the NumberSpinner instance.
       numberSpinnerOptions: {
         font: new PhetFont( 26 ),
         backgroundMinWidth: 37,
@@ -57,56 +70,49 @@ class PlayAreaControlSet extends VBox {
         yMargin: 4
       },
 
-      // {boolean} - indicates if a grid checkbox should be included.
-      includeGridCheckbox: true,
-
-      // {Object} - options passed to the NumberSpinner instance
-      gridCheckboxOptions: merge( {}, CollisionLabConstants.CHECKBOX_OPTIONS, {
-        gridSize: 26
-      } ),
-
-      // {number} - y-margins between the title, number spinner, and grid checkbox, respectively.
-      titleToNumberSpinnerMargin: 6,
-      numberSpinnerToGridCheckboxMargin: 12,
-
       // super-class options
       align: 'left'
-
     }, options );
 
-    // Set the spacing super-class option.
+    // Set options that cannot be overridden.
     assert && assert( !options.spacing, 'PlayAreaControlSet sets spacing' );
+    assert && assert( !options.children, 'PlayAreaControlSet sets children' );
     options.spacing = options.numberSpinnerToGridCheckboxMargin;
-
-    super( options );
+    options.children = [];
 
     //----------------------------------------------------------------------------------------
 
-    // Create the title Text of the PlayAreaControlSet.
-    const ballsText = new Text( collisionLabStrings.balls, options.titleTextOptions );
+    if ( options.includeNumberOfBallsSpinner ) {
 
-    // Create the NumberSpinner of the PlayAreaControlSet to allow the user to change the number of balls.
-    const numberOfBallsSpinner = new NumberSpinner( numberOfBallsProperty,
-      new Property( numberOfBallsRange ),
-      options.numberSpinnerOptions );
+      // Create the title Text of the PlayAreaControlSet.
+      const ballsText = new Text( collisionLabStrings.balls, options.titleTextOptions );
 
-    // Wrap the controls of the number of Balls in a separate VBox to give separate spacing.
-    const ballControlBox = new VBox( {
-      children: [ ballsText, numberOfBallsSpinner ],
-      spacing: options.titleToNumberSpinnerMargin
-    } );
+      // Create the NumberSpinner of the PlayAreaControlSet to allow the user to change the number of balls.
+      const numberOfBallsSpinner = new NumberSpinner( numberOfBallsProperty,
+        new Property( numberOfBallsRange ),
+        options.numberSpinnerOptions );
 
-    this.addChild( ballControlBox );
+      // Wrap the controls of the number of Balls in a separate VBox to give separate spacing.
+      const ballControlBox = new VBox( {
+        children: [ ballsText, numberOfBallsSpinner ],
+        spacing: options.titleToNumberSpinnerMargin
+      } );
+
+      options.children.push( ballControlBox );
+    }
 
     //----------------------------------------------------------------------------------------
 
     // Create and add a GridCheckbox instance if includeGridCheckbox is true.
     if ( options.includeGridCheckbox ) {
-      this.addChild( new GridCheckbox( gridVisibleProperty, options.gridCheckboxOptions ) );
+
+      // Create the Grid Checkbox.
+      const gridCheckbox = new GridCheckbox( gridVisibleProperty, options.gridCheckboxOptions );
+
+      options.children.push( gridCheckbox );
     }
 
-    // Apply any additional Bounds mutators.
-    this.mutate( options );
+    super( options );
   }
 }
 
