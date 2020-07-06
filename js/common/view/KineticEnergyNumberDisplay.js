22a3dd187d4028bb4a100c61ca7aa9e23bc6a407
Brandon Li <brandon.li820@icloud.com>
2020-07-06 15:01:31 -0600

    Clean up Kinetic Energy Number Display

--- js/common/view/KineticEnergyNumberDisplay.js
+++ js/common/view/KineticEnergyNumberDisplay.js
@@ -1,15 +1,20 @@
 // Copyright 2019-2020, University of Colorado Boulder
 
 /**
- * The numerical display for the total kinetic energy.
+ * KineticEnergyNumberDisplay is a subclass of PlayAreaNumberDisplay for displaying the total kinetic energy of a
+ * BallSystem. Instances are positioned just inside the bottom-right corner of the PlayArea and appears on all
+ * Screens.
+ *
+ * KineticEnergyNumberDisplays are created at the start of the sim and are never disposed, so no dispose method is
+ * necessary.
  *
  * @author Brandon Li
  * @author Martin Veillette
  */
 
-import Range from '../../../../dot/js/Range.js';
 import merge from '../../../../phet-core/js/merge.js';
 import AssertUtils from '../../../../phetcommon/js/AssertUtils.js';
+import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
 import collisionLab from '../../collisionLab.js';
 import collisionLabStrings from '../../collisionLabStrings.js';
 import PlayAreaNumberDisplay from './PlayAreaNumberDisplay.js';
@@ -26,13 +31,23 @@ class KineticEnergyNumberDisplay extends PlayAreaNumberDisplay {
 
     options = merge( {
 
-      valuePattern: collisionLabStrings.kineticEnergyJ,
-      maxWidth: 300, // constrain width for i18n, determined empirically.
-      displayRange: new Range( 0, 10 ) // Range for determining width, determined empirically.
+      valuePattern: StringUtils.fillIn( collisionLabStrings.pattern.labelEqualsValueUnits, {
+        label: collisionLabStrings.kineticEnergy,
+        units: collisionLabStrings.units.joules
+      } ),
+      maxWidth: 300 // constrain width for i18n, determined empirically.
 
     }, options );
 
     super( totalKineticEnergyProperty, kineticEnergyVisibleProperty, options );
+  }
+
+  /**
+   * @public
+   * @override
+   */
+  dispose() {
+    assert && assert( false, 'KineticEnergyNumberDisplay is not intended to be disposed' );
   }
 }
 
