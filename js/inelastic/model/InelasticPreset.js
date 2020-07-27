926d77b462cd958d0a91069f27951d5a8768a052
Brandon Li <brandon.li820@icloud.com>
2020-07-27 14:56:31 -0600

    Add third preset

--- js/inelastic/model/InelasticPreset.js
+++ js/inelastic/model/InelasticPreset.js
@@ -78,6 +78,11 @@ const InelasticPreset = Enumeration.byMap( {
   NAME_2: new InelasticPresetValue( [
     new BallState( new Vector2( -0.5, 0.00 ), new Vector2( 0.50, 0 ), 0.5 ),
     new BallState( new Vector2( 0.50, 0.00 ), new Vector2( -0.5, 0 ), 0.5 )
+  ] ),
+
+  NAME_3: new InelasticPresetValue( [
+    new BallState( new Vector2( -0.65, 0.00 ), new Vector2( 0, 0 ), 1.8 ),
+    new BallState( new Vector2( 0.6, 0.12 ), new Vector2( -1, 0 ), 0.3 )
   ] )
 
 } );
