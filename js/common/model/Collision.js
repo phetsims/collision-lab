85c1090cbb70e7a77d73c44ee52dcba4b88cd33c
Brandon Li <brandon.li820@icloud.com>
2020-08-07 11:12:49 -0600

    #126 change Collision to work in terms of elapsed time

--- js/common/model/Collision.js
+++ js/common/model/Collision.js
@@ -5,7 +5,9 @@
  * collision, the object the Ball is colliding with, and at what time the collision will occur. Doesn't hold onto any
  * listeners or Properties, so no dispose method is needed.
  *
- * The Collision data-structure is used to encapsulate the necessary information of a potential collision that may
+ *
+ *
+ * TODO: update? The Collision data-structure is used to encapsulate the necessary information of a potential collision that may
  * happen in the future. CollisionEngine will detect all potential ball-to-ball and ball-to-border collisions upfront
  * and construct Collision instances, if needed. Then, it uses the data that each Collision holds onto to respond to the
  * next collision and repeats the process until there are no collisions detected within the time-step.
@@ -22,12 +24,12 @@ class Collision {
   /**
    * @param {Ball} ball - the Ball that is involved in the collision.
    * @param {Ball|PlayArea} collidingObject - the object the Ball is colliding with.
-   * @param {number} collisionTime - the amount of time until the collision will occur.
+   * @param {number} time - the elapsed time of when the collision will occur.
    */
-  constructor( ball, collidingObject, collisionTime ) {
+  constructor( ball, collidingObject, time ) {
     assert && assert( ball instanceof Ball, `invalid ball: ${ball}` );
-    assert && assert( collidingObject instanceof Ball || collidingObject instanceof PlayArea, `invalid collidingObject: ${collidingObject}` );
-    assert && assert( typeof collisionTime === 'number', `invalid collisionTime: ${collisionTime}` );
+    assert && assert( collidingObject instanceof Ball || collidingObject instanceof PlayArea );
+    assert && assert( typeof time === 'number', `invalid time: ${time}` );
 
     // @public {Ball} - reference to the passed-in ball.
     this.ball = ball;
@@ -35,8 +37,8 @@ class Collision {
     // @public {Ball|PlayArea} - reference to the passed-in collidingObject.
     this.collidingObject = collidingObject;
 
-    // @public {number} - reference to the passed-in collisionTime.
-    this.collisionTime = collisionTime;
+    // @public {number} - reference to the passed-in time.
+    this.time = time;
   }
 
   /**
@@ -46,7 +48,7 @@ class Collision {
    * @returns {string}
    */
   toString() {
-    return `Collision[ ball: ${this.ball} colliding with: ${this.collidingObject}, in ${this.collisionTime} s ]`;
+    return `Collision[ ball: ${this.ball} colliding with: ${this.collidingObject}, at ${this.time} s ]`;
   }
 }
 
