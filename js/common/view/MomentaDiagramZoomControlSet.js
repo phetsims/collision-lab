f46f40b60321d7a46aeee328bc60b6519b7ea12c
Jesse <jesse.greenberg@colorado.edu>
2020-11-23 17:06:24 -0500

    rename radius option to magnifyingGlassRadius in ZoomButton, see phetsims/scenery-phet#645

--- js/common/view/MomentaDiagramZoomControlSet.js
+++ js/common/view/MomentaDiagramZoomControlSet.js
@@ -33,7 +33,7 @@ class MomentaDiagramZoomControlSet extends HBox {
       // {Object} - passed to both ZoomButton instances.
       zoomButtonOptions: {
         baseColor: ColorConstants.LIGHT_BLUE,
-        radius: 8,
+        magnifyingGlassRadius: 8,
         xMargin: 5,
         yMargin: 3,
         touchAreaXDilation: 3.5,
