a43a5df32c847580deedf3a35a4b71473dd84db8
Jonathan Olson <jonathan.olson@colorado.edu>
2020-12-09 21:54:12 -0700

    Collision detection handling for https://github.com/phetsims/collision-lab/issues/184

--- js/explore1D/model/Explore1DModel.js
+++ js/explore1D/model/Explore1DModel.js
@@ -9,6 +9,7 @@
 import collisionLab from '../../collisionLab.js';
 import CollisionLabModel from '../../common/model/CollisionLabModel.js';
 import Explore1DBallSystem from './Explore1DBallSystem.js';
+import Explore1DCollisionEngine from './Explore1DCollisionEngine.js';
 import Explore1DPlayArea from './Explore1DPlayArea.js';
 
 class Explore1DModel extends CollisionLabModel {
@@ -38,6 +39,23 @@ class Explore1DModel extends CollisionLabModel {
     assert && assert( playArea instanceof Explore1DPlayArea, `invalid playArea: ${playArea}` );
 
     return new Explore1DBallSystem( playArea );
+  }
+
+  /**
+   * Creates the CollisionEngine for the 'Inelastic' screen, which uses a screen-specific sub-type of CollisionEngine.
+   * Called in the constructor of the super-class, which uses the Factory Method Pattern.
+   * @override
+   * @protected
+   *
+   * @param {Explore1DPlayArea} playArea
+   * @param {Explore1DBallSystem} ballSystem
+   * @returns {Explore1DCollisionEngine}
+   */
+  createCollisionEngine( playArea, ballSystem ) {
+    assert && assert( playArea instanceof Explore1DPlayArea, `invalid playArea: ${playArea}` );
+    assert && assert( ballSystem instanceof Explore1DBallSystem, `invalid ballSystem: ${ballSystem}` );
+
+    return new Explore1DCollisionEngine( playArea, ballSystem );
   }
 }
 
