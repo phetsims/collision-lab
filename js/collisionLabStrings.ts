002a5860d82423068c4588f7097862ecb62e00a7
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:02 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/collisionLabStrings.ts
@@ -0,0 +1,76 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import collisionLab from './collisionLab.js';
+
+type StringsType = {
+  'collision-lab': {
+    'title': string
+  },
+  'screen': {
+    'intro': string,
+    'explore1D': string,
+    'explore2D': string,
+    'inelastic': string
+  },
+  'pattern': {
+    'range': string,
+    'labelParenthesesUnits': string,
+    'valueUnits': string,
+    'valueSpaceUnits': string,
+    'symbolSubSymbol': string,
+    'labelEqualsValueUnits': string,
+    'labelEqualsValueSpaceUnits': string,
+    'vectorSymbolEqualsValueSpaceUnits': string
+  },
+  'units': {
+    'kilograms': string,
+    'meters': string,
+    'seconds': string,
+    'metersPerSecond': string,
+    'kilogramMetersPerSecond': string,
+    'percent': string,
+    'joules': string
+  },
+  'symbol': {
+    'x': string,
+    'y': string,
+    'momentum': string,
+    'velocity': string
+  },
+  'mass': string,
+  'position': string,
+  'velocity': string,
+  'momentum': string,
+  'path': string,
+  'total': string,
+  'elasticity': string,
+  'inelastic': string,
+  'elastic': string,
+  'values': string,
+  'momentaDiagram': string,
+  'reflectingBorder': string,
+  'kineticEnergy': string,
+  'centerOfMass': string,
+  'constantSize': string,
+  'stick': string,
+  'slip': string,
+  'moreData': string,
+  'balls': string,
+  'enter': string,
+  'returnBalls': string,
+  'changeInMomentum': string,
+  'custom': string,
+  'inelasticCollision': string,
+  'approximatePattern': string
+};
+
+const collisionLabStrings = getStringModule( 'COLLISION_LAB' ) as StringsType;
+
+collisionLab.register( 'collisionLabStrings', collisionLabStrings );
+
+export default collisionLabStrings;
