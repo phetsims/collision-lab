5a5eea16ab8d10a932d5e6ea2967c1a47c080c4a
phet-dev <phetadmin@gmail.com>
2022-04-20 03:27:24 -0600

    update copyright dates from daily grunt work

--- js/common/view/CollisionLabTimeControlNode.js
+++ js/common/view/CollisionLabTimeControlNode.js
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * CollisionLabTimeControlNode is a subclass of TimeControlNode, specialized for the 'Collision Lab' simulation.
