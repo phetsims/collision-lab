df3fdec29e42fadb2a8cf5ab962375051f0a3f2f
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:39 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/collisionLabStrings.ts
+++ js/collisionLabStrings.ts
@@ -5,68 +5,118 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import collisionLab from './collisionLab.js';
 
 type StringsType = {
   'collision-lab': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'intro': string;
+    'introProperty': TReadOnlyProperty<string>;
     'explore1D': string;
+    'explore1DProperty': TReadOnlyProperty<string>;
     'explore2D': string;
+    'explore2DProperty': TReadOnlyProperty<string>;
     'inelastic': string;
+    'inelasticProperty': TReadOnlyProperty<string>;
   };
   'pattern': {
     'range': string;
+    'rangeProperty': TReadOnlyProperty<string>;
     'labelParenthesesUnits': string;
+    'labelParenthesesUnitsProperty': TReadOnlyProperty<string>;
     'valueUnits': string;
+    'valueUnitsProperty': TReadOnlyProperty<string>;
     'valueSpaceUnits': string;
+    'valueSpaceUnitsProperty': TReadOnlyProperty<string>;
     'symbolSubSymbol': string;
+    'symbolSubSymbolProperty': TReadOnlyProperty<string>;
     'labelEqualsValueUnits': string;
+    'labelEqualsValueUnitsProperty': TReadOnlyProperty<string>;
     'labelEqualsValueSpaceUnits': string;
+    'labelEqualsValueSpaceUnitsProperty': TReadOnlyProperty<string>;
     'vectorSymbolEqualsValueSpaceUnits': string;
+    'vectorSymbolEqualsValueSpaceUnitsProperty': TReadOnlyProperty<string>;
   };
   'units': {
     'kilograms': string;
+    'kilogramsProperty': TReadOnlyProperty<string>;
     'meters': string;
+    'metersProperty': TReadOnlyProperty<string>;
     'seconds': string;
+    'secondsProperty': TReadOnlyProperty<string>;
     'metersPerSecond': string;
+    'metersPerSecondProperty': TReadOnlyProperty<string>;
     'kilogramMetersPerSecond': string;
+    'kilogramMetersPerSecondProperty': TReadOnlyProperty<string>;
     'percent': string;
+    'percentProperty': TReadOnlyProperty<string>;
     'joules': string;
+    'joulesProperty': TReadOnlyProperty<string>;
   };
   'symbol': {
     'x': string;
+    'xProperty': TReadOnlyProperty<string>;
     'y': string;
+    'yProperty': TReadOnlyProperty<string>;
     'momentum': string;
+    'momentumProperty': TReadOnlyProperty<string>;
     'velocity': string;
+    'velocityProperty': TReadOnlyProperty<string>;
   };
   'mass': string;
+  'massProperty': TReadOnlyProperty<string>;
   'position': string;
+  'positionProperty': TReadOnlyProperty<string>;
   'velocity': string;
+  'velocityProperty': TReadOnlyProperty<string>;
   'momentum': string;
+  'momentumProperty': TReadOnlyProperty<string>;
   'path': string;
+  'pathProperty': TReadOnlyProperty<string>;
   'total': string;
+  'totalProperty': TReadOnlyProperty<string>;
   'elasticity': string;
+  'elasticityProperty': TReadOnlyProperty<string>;
   'inelastic': string;
+  'inelasticProperty': TReadOnlyProperty<string>;
   'elastic': string;
+  'elasticProperty': TReadOnlyProperty<string>;
   'values': string;
+  'valuesProperty': TReadOnlyProperty<string>;
   'momentaDiagram': string;
+  'momentaDiagramProperty': TReadOnlyProperty<string>;
   'reflectingBorder': string;
+  'reflectingBorderProperty': TReadOnlyProperty<string>;
   'kineticEnergy': string;
+  'kineticEnergyProperty': TReadOnlyProperty<string>;
   'centerOfMass': string;
+  'centerOfMassProperty': TReadOnlyProperty<string>;
   'constantSize': string;
+  'constantSizeProperty': TReadOnlyProperty<string>;
   'stick': string;
+  'stickProperty': TReadOnlyProperty<string>;
   'slip': string;
+  'slipProperty': TReadOnlyProperty<string>;
   'moreData': string;
+  'moreDataProperty': TReadOnlyProperty<string>;
   'balls': string;
+  'ballsProperty': TReadOnlyProperty<string>;
   'enter': string;
+  'enterProperty': TReadOnlyProperty<string>;
   'returnBalls': string;
+  'returnBallsProperty': TReadOnlyProperty<string>;
   'changeInMomentum': string;
+  'changeInMomentumProperty': TReadOnlyProperty<string>;
   'custom': string;
+  'customProperty': TReadOnlyProperty<string>;
   'inelasticCollision': string;
+  'inelasticCollisionProperty': TReadOnlyProperty<string>;
   'approximatePattern': string;
+  'approximatePatternProperty': TReadOnlyProperty<string>;
 };
 
 const collisionLabStrings = getStringModule( 'COLLISION_LAB' ) as StringsType;
