87766242ac94048c2e23628e6ce6c5efe22d273e
phet-dev <phetadmin@gmail.com>
2023-07-11 03:29:26 -0600

    update copyright dates from daily grunt work

--- js/common/view/ElapsedTimeNumberDisplay.js
+++ js/common/view/ElapsedTimeNumberDisplay.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2023, University of Colorado Boulder
 
 /**
  * ElapsedTimeNumberDisplay is a subclass of NumberDisplay for displaying the total elapsed time of the sim.
--- js/common/view/KineticEnergyNumberDisplay.js
+++ js/common/view/KineticEnergyNumberDisplay.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2023, University of Colorado Boulder
 
 /**
  * KineticEnergyNumberDisplay is a subclass of PlayAreaNumberDisplay for displaying the total kinetic energy of a
