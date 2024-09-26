2a4576c2103fcaab85f7f0c308e46f507e3ab19b
Sam Reid <reids@colorado.edu>
2024-09-25 19:32:36 -0600

    Upgrade to ESLint 9 and flat config, see https://github.com/phetsims/chipper/issues/1474

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+// Copyright 2024, University of Colorado Boulder
+
+/**
+ * ESlint configuration for collision-lab.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+import parent from '../chipper/eslint/sim.eslint.config.mjs';
+
+export default [
+  ...parent,
+  {
+    languageOptions: {
+      globals: {
+        sceneryLog: 'readonly'
+      }
+    }
+  }
+];
