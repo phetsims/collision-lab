8bace08f349652e4e66ac00808cec630923577b1
phet-dev <phetadmin@gmail.com>
2024-11-19 03:18:40 -0700

    update copyright dates from daily grunt work

--- js/common/view/CollisionLabControlPanel.js
+++ js/common/view/CollisionLabControlPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2023, University of Colorado Boulder
+// Copyright 2019-2024, University of Colorado Boulder
 
 /**
  * Control Panel at the upper-right corner of each screen that allows the user to manipulate certain Properties of
